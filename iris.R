4d7350bb422da9128b860e60c348af36d2a592d1
t.sanchez <teresaschezmejia@gmail.com>
2024-02-07 11:51:02 +0100

    added plotting

--- iris.R
+++ iris.R
@@ -4,3 +4,9 @@ iris_data <- iris
 
 iris_versicolor <- iris |>
   filter(Species == "versicolor")
+
+library(ggplot2)
+
+ggplot(iris_setosa, aes(x = Petal.Length, y = Sepal.Length)) +
+  geom_point() +
+  geom_smooth(method = "lm")
